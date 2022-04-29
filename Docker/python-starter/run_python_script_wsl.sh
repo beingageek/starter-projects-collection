3e0f6f2f513a872f3e2f318afeac827ba6a405f2
Rishikanta Pukhrambam <7808756+beingageek@users.noreply.github.com>
2022-04-28 22:10:49 -0700

    Changed to Python slim package

--- Docker/python-starter/run_python_script_wsl.sh
+++ Docker/python-starter/run_python_script_wsl.sh
@@ -1,2 +1,2 @@
 # Run a single Python script without creating a docker image
-docker run -it --rm --name python-app -v "$PWD":/usr/src/app -w /usr/src/app python:latest python python_hello_world.py
+docker run -it --rm --name python-app -v "$PWD":/usr/src/app -w /usr/src/app python:3-slim python python_hello_world.py
